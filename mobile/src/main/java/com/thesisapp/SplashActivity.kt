36509b73be45833671e9abed2d018de1069a9199
A2-ard <jan_relucio@dlsu.edu.ph>
2025-06-09 11:23:11 +0800

    Programs Connecting FrontEnds 0.0

--- mobile/src/main/java/com/thesisapp/SplashActivity.kt
+++ mobile/src/main/java/com/thesisapp/SplashActivity.kt
@@ -7,15 +7,16 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // Delay and go to MainDashboardActivity
+        // Delay for 3 seconds, then move to MainDashboardActivity
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainDashboardActivity::class.java)
             startActivity(intent)
-            finish() // Close splash so back button doesn't return here
-        }, 2000) // 2000ms = 2 seconds
+            finish() // Prevent user from returning to splash
+        }, 3000) // 3000 milliseconds = 3 seconds
     }
 }
