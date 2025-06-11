cb6d15a9d214a1af827d66acf869378e9a14fb75
A2-ard <jan_relucio@dlsu.edu.ph>
2025-06-11 11:57:43 +0800

    Programs Connecting FrontEnds 0.1

---
+++ mobile/src/main/java/com/thesisapp/ConnectPromptActivity.kt
@@ -0,0 +1,11 @@
+package com.thesisapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConnectPromptActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.connect_prompt)
+    }
+}
